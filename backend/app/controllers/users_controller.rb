69447ded160b06c4734b687a2c8516129156ed79
Maruf <marufbinawal@gmail.com>
2019-07-25 11:21:06 -0500

    added routes, modesl, and controllers, need to create migrations and database

---
+++ backend/app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UserController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def create
+        
+        user = User.create(user_params)
+        render json: user
+
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def update
+        # Find the user
+        # Save params in the database -> Update the user we find
+        user = User.find(params[:id])
+        user.update(user_params)
+    end
+
+    def new
+    end
+
+    def user_params
+        params.permit(
+            :name
+            :password
+        )
+    end
+end
