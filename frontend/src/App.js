e84ad2a1f9b2f23f0d9c01943ad44743ec874096
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-07-25 17:44:57 -0500

    base structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Productlist from './components/Productlist';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+          <Switch>
+            <Route exact path="/" component={Productlist}></Route>
+            <Route path="/details" component={Details}></Route>
+            <Route path="/cart" component={Cart}></Route>
+            <Route component={Default}></Route>
+          </Switch>
+      </React.Fragment>
+   
+    );
+  }
 }
 
 export default App;
